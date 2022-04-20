189a7ea11135817a5fe648f86fb3b0905b24f194
miiqbal <arizkyiqbal@gmail.com>
2022-04-20 21:33:58 +0700

    Add Tugas 2 Workshop

---
+++ lib/tugas2/tugas.dart
@@ -0,0 +1,278 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        centerTitle: true,
+        leading: const Icon(
+          Icons.arrow_drop_down_circle_outlined,
+          color: Colors.blueAccent,
+          size: 18.0,
+        ),
+        title: const Text(
+          "Products",
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontSize: 25,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.only(top: 4.5, right: 5, left: 5),
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(12)),
+                      color: Colors.grey[100],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Image.asset(
+                              "assets/images/shoe.jpg",
+                              height: 125,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Brand new shoes",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 22),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 6.0),
+                                  child: Text(
+                                    "SH-231",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                      color: Colors.red,
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.only(
+                                          top: 8.0,
+                                          bottom: 8,
+                                          left: 20,
+                                          right: 20),
+                                      child: Text("Stock 1",
+                                          style:
+                                              TextStyle(color: Colors.white)),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 100.0, bottom: 100),
+                            child: Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                              size: 28.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(12)),
+                      color: Colors.grey[100],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Image.asset(
+                              "assets/images/shoe.jpg",
+                              height: 125,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Brand new shoes",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 22),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 6.0),
+                                  child: Text(
+                                    "SH-231",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                      color: Colors.green,
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.only(
+                                          top: 8.0,
+                                          bottom: 8,
+                                          left: 20,
+                                          right: 20),
+                                      child: Text("Stock 1",
+                                          style:
+                                              TextStyle(color: Colors.white)),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 100.0, bottom: 100),
+                            child: Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                              size: 28.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(12)),
+                      color: Colors.grey[100],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Image.asset(
+                              "assets/images/shoe.jpg",
+                              height: 125,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Brand new shoes",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 22),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 6.0),
+                                  child: Text(
+                                    "SH-231",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                      color: Colors.green,
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.only(
+                                          top: 8.0,
+                                          bottom: 8,
+                                          left: 20,
+                                          right: 20),
+                                      child: Text("Stock 10",
+                                          style:
+                                              TextStyle(color: Colors.white)),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 100.0, bottom: 100),
+                            child: Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                              size: 28.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+            ],
+          )),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text('New Product'),
+        icon: const Icon(Icons.shopping_bag_outlined),
+        backgroundColor: Colors.blueAccent,
+      ),
+    );
+  }
+}
